2521ee71874d42d8b9fd59545cff3bc6940d4fc0
deva0914 <d.y.virgo0914@gmail.com>
2021-05-06 02:25:57 +0900

    [add] 暫定完成版を追加．

---
+++ Image2Base64/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Image2Base64
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dropPanel = new System.Windows.Forms.Panel();
+            this.base64TextBox = new System.Windows.Forms.TextBox();
+            this.copyButton = new System.Windows.Forms.Button();
+            this.preTextBox = new System.Windows.Forms.TextBox();
+            this.preTextLabel = new System.Windows.Forms.Label();
+            this.postTextLabel = new System.Windows.Forms.Label();
+            this.postTextBox = new System.Windows.Forms.TextBox();
+            this.reloadButton = new System.Windows.Forms.Button();
+            this.urlCheckBox = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // dropPanel
+            // 
+            this.dropPanel.AllowDrop = true;
+            this.dropPanel.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.dropPanel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dropPanel.Location = new System.Drawing.Point(12, 12);
+            this.dropPanel.Name = "dropPanel";
+            this.dropPanel.Size = new System.Drawing.Size(760, 129);
+            this.dropPanel.TabIndex = 0;
+            this.dropPanel.DragDrop += new System.Windows.Forms.DragEventHandler(this.DropPanel_DragDrop);
+            this.dropPanel.DragEnter += new System.Windows.Forms.DragEventHandler(this.DropPanel_DragEnter);
+            // 
+            // base64TextBox
+            // 
+            this.base64TextBox.HideSelection = false;
+            this.base64TextBox.Location = new System.Drawing.Point(12, 300);
+            this.base64TextBox.Multiline = true;
+            this.base64TextBox.Name = "base64TextBox";
+            this.base64TextBox.ReadOnly = true;
+            this.base64TextBox.Size = new System.Drawing.Size(760, 100);
+            this.base64TextBox.TabIndex = 1;
+            // 
+            // copyButton
+            // 
+            this.copyButton.Location = new System.Drawing.Point(697, 406);
+            this.copyButton.Name = "copyButton";
+            this.copyButton.Size = new System.Drawing.Size(75, 23);
+            this.copyButton.TabIndex = 2;
+            this.copyButton.Text = "コピーする";
+            this.copyButton.UseVisualStyleBackColor = true;
+            this.copyButton.Click += new System.EventHandler(this.CopyButton_Click);
+            // 
+            // preTextBox
+            // 
+            this.preTextBox.Location = new System.Drawing.Point(12, 196);
+            this.preTextBox.Name = "preTextBox";
+            this.preTextBox.Size = new System.Drawing.Size(763, 19);
+            this.preTextBox.TabIndex = 3;
+            // 
+            // preTextLabel
+            // 
+            this.preTextLabel.AutoSize = true;
+            this.preTextLabel.Location = new System.Drawing.Point(10, 181);
+            this.preTextLabel.Name = "preTextLabel";
+            this.preTextLabel.Size = new System.Drawing.Size(65, 12);
+            this.preTextLabel.TabIndex = 4;
+            this.preTextLabel.Text = "接頭テキスト";
+            // 
+            // postTextLabel
+            // 
+            this.postTextLabel.AutoSize = true;
+            this.postTextLabel.Location = new System.Drawing.Point(13, 235);
+            this.postTextLabel.Name = "postTextLabel";
+            this.postTextLabel.Size = new System.Drawing.Size(65, 12);
+            this.postTextLabel.TabIndex = 6;
+            this.postTextLabel.Text = "接尾テキスト";
+            // 
+            // postTextBox
+            // 
+            this.postTextBox.Location = new System.Drawing.Point(12, 250);
+            this.postTextBox.Name = "postTextBox";
+            this.postTextBox.Size = new System.Drawing.Size(763, 19);
+            this.postTextBox.TabIndex = 5;
+            // 
+            // reloadButton
+            // 
+            this.reloadButton.Location = new System.Drawing.Point(616, 406);
+            this.reloadButton.Name = "reloadButton";
+            this.reloadButton.Size = new System.Drawing.Size(75, 23);
+            this.reloadButton.TabIndex = 7;
+            this.reloadButton.Text = "リロードする";
+            this.reloadButton.UseVisualStyleBackColor = true;
+            this.reloadButton.Click += new System.EventHandler(this.ReloadButton_Click);
+            // 
+            // urlCheckBox
+            // 
+            this.urlCheckBox.AutoSize = true;
+            this.urlCheckBox.Location = new System.Drawing.Point(12, 147);
+            this.urlCheckBox.Name = "urlCheckBox";
+            this.urlCheckBox.Size = new System.Drawing.Size(127, 16);
+            this.urlCheckBox.TabIndex = 8;
+            this.urlCheckBox.Text = "データURLで出力する";
+            this.urlCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.urlCheckBox);
+            this.Controls.Add(this.reloadButton);
+            this.Controls.Add(this.postTextLabel);
+            this.Controls.Add(this.postTextBox);
+            this.Controls.Add(this.preTextLabel);
+            this.Controls.Add(this.preTextBox);
+            this.Controls.Add(this.copyButton);
+            this.Controls.Add(this.base64TextBox);
+            this.Controls.Add(this.dropPanel);
+            this.Name = "Form1";
+            this.Text = "Image 2 Base64";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel dropPanel;
+        private System.Windows.Forms.TextBox base64TextBox;
+        private System.Windows.Forms.Button copyButton;
+        private System.Windows.Forms.TextBox preTextBox;
+        private System.Windows.Forms.Label preTextLabel;
+        private System.Windows.Forms.Label postTextLabel;
+        private System.Windows.Forms.TextBox postTextBox;
+        private System.Windows.Forms.Button reloadButton;
+        private System.Windows.Forms.CheckBox urlCheckBox;
+    }
+}
+
---
+++ Image2Base64/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Image2Base64
+{
+    public partial class Form1 : System.Windows.Forms.Form
+    {
+        private string fileName = "";
+        private string fileExt = "";
+        private string imageBytesText = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// ドロップされたファイルをbase64形式に変換する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DropPanel_DragDrop(object sender, DragEventArgs e)
+        {
+            Bitmap image = null;
+
+            try
+            {
+                // ドロップされたファイルの1枚のみ取得
+                string[] files = (string[])e.Data.GetData(DataFormats.FileDrop, false);
+                if (files.Length > 1) { MessageBox.Show("画像は最初の1枚だけ選択されます．", "注意", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); }
+
+                // ファイルの情報(名前と拡張子)を抜き出す
+                string file = files[0];
+                fileName = System.IO.Path.GetFileName(file);    // 例：sample.jpeg
+                fileExt = System.IO.Path.GetExtension(file);    // 例：.jpeg
+
+                // イメージに落とし込み，メモリストリームからbase64に変換
+                image = new Bitmap(file);
+                var memoryStream = new System.IO.MemoryStream();
+                image.Save(memoryStream, System.Drawing.Imaging.ImageFormat.Jpeg);
+                byte[] imageBytes = memoryStream.ToArray();
+                imageBytesText = Convert.ToBase64String(imageBytes);
+
+                // テキストボックスに出力
+                OutputText();
+            }
+            catch
+            {
+                MessageBox.Show("エラーが発生しました．アプリケーションを終了します．", "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            finally
+            {
+                // リソースの解放
+                if (image != null) { image.Dispose(); }
+            }
+        }
+
+        /// <summary>
+        /// テキストを指定された形式でテキストボックスに書き込む
+        /// </summary>
+        private void OutputText()
+        {
+            var sb = new StringBuilder();
+            if (urlCheckBox.Checked)
+            {
+                sb.Append("[");
+                sb.Append(fileName);
+                sb.Append("](data:image/");
+                sb.Append(fileExt.Substring(1));    // 先頭のピリオド(.)を取る
+                sb.Append(";base64,");
+            }
+            sb.Append(preTextBox.Text);
+            sb.Append(imageBytesText);
+            sb.Append(postTextBox.Text);
+            if (urlCheckBox.Checked) { sb.Append(")"); }
+
+            base64TextBox.Text = sb.ToString();
+        }
+
+        /// <summary>
+        /// ファイルをドロップしようとした時のみ，カーソルアイコンを変更する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DropPanel_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// テキストボックスのテキストをクリップボードにコピーする
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CopyButton_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(base64TextBox.Text);
+        }
+
+        /// <summary>
+        /// URLや接頭・接尾テキストなどの設定を基に，テキストを再構成する
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ReloadButton_Click(object sender, EventArgs e)
+        {
+            OutputText();
+        }
+    }
+}
